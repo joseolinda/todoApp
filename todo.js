298a217500b26500715a6a402bc50410dbedc988
José Olinda <joseolindad@yahoo.com.br>
2019-07-31 15:58:01 -0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -6,6 +6,6 @@ const listaFazer = document.querySelector(".todo-list ul");
 const listaFeito =  document.querySelector(".complete-list ul");
 
 // Funcao Adicionar Tareta
-var createNewTask = function(task){
+const createNewTask = function(task){
   console.log("Creating ..." + task);
 }
