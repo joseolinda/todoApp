aac16f71a4847cdb701c79b16bd9bd61a8e78e00
José Olinda <joseolindad@yahoo.com.br>
2019-07-31 15:39:54 -0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,3 +1,3 @@
 // Guardar os elementos em variáveis
 const newTask = document.querySelector('#new-task');
-conts addTaskBtn = document.querySelector('#addTask');
+const addTaskBtn = document.querySelector('#addTask');
